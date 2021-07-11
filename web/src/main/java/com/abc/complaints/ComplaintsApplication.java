a1d872411ae9bb037340320fbf6a33e18f65c564
MarwaTaha95 <marwataha203@gmail.com>
2021-07-11 19:27:44 +0300

    Separate the backend project into multiple sub-projects

--- web/src/main/java/com/abc/complaints/ComplaintsApplication.java
+++ web/src/main/java/com/abc/complaints/ComplaintsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ComplaintsApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ComplaintsApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintsApplication.class, args);
+	}
 
 }
